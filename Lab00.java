d2482f523a34d5a72366089543fa5f15f6cba9a5
Hannah Allen <hsallen@unix4.csc.calpoly.edu>
2018-09-24 12:50:52 -0700

    Final

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,40 @@
+// Hannah Allen
+// CPE 203-22
+
+
 public class Lab00
 {
-   public static void main(String[] args)
-   {
+   public static void main(String[] args) {
+      // declaring and initializing some variables
+      int x = 5;
+      String y = "hello";
+      double z = 9.8;
+      
+      // printing the variables
+      System.out.println("x:" + " " + x + " " + "y:" + " " + y + " " + "z:" + " " + z);
 
-   
+      // a list (make an array in java)
+      int[] nums = {3, 6, -1, 2};
+      for (int num : nums) 
+         System.out.println(num);
+
+      // call a function
+      int numFound = char_count(y, 'l');
+      System.out.println("Found:" + " " + numFound);
+
+      // a counting for loop
+      for (int i=1; i<11; i++) 
+         System.out.print(i + " ");
+
+      System.out.println();
+   }
+   public static int char_count(String s, char c) {
+      int count = 0;
+      char[] charArray = s.toCharArray();
+      for (char ch : charArray) {
+         if (ch == c)
+            count += 1;
+      }
+      return count;
    }
 }
